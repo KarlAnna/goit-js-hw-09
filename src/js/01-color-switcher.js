34b8ef254448f8c3fb61cb23ffa98ee5730ded23
KarlAnna <106392024+KarlAnna@users.noreply.github.com>
2022-11-04 10:05:02 +0200

    task 1 was completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('button[data-start]')
+const stopBtn = document.querySelector('button[data-stop]')
+const body = document.querySelector('body')
+let intervalId = null
 
+startBtn.addEventListener('click', () => {
+  intervalId = setInterval(updateBodyBgColor, 1000)
+})
+
+
+function updateBodyBgColor() {
+  body.style.backgroundColor = getRandomHexColor()
+  startBtn.setAttribute('disabled', true)
+  stopBtn.removeAttribute('disabled')
+}
+
+stopBtn.addEventListener('click', onStopBtnClick)
+function onStopBtnClick() {
+  clearInterval(intervalId)
+  startBtn.removeAttribute('disabled')
+  stopBtn.setAttribute('disabled', true)
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
