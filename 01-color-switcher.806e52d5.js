0a211dcd79c490d50614a99a1d381c1850e87eb6
KarlAnna <106392024+KarlAnna@users.noreply.github.com>
2022-11-04 08:05:24 +0000

    Deploying to gh-pages from @ KarlAnna/goit-js-hw-09@34b8ef254448f8c3fb61cb23ffa98ee5730ded23 🚀

---
+++ 01-color-switcher.806e52d5.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;function r(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),t.setAttribute("disabled",!0),e.removeAttribute("disabled")}t.addEventListener("click",(function(){o=setInterval(r,1e3)})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}))}();
+//# sourceMappingURL=01-color-switcher.806e52d5.js.map
